70f1f5a8d4d6e5b42dae060a8d9596f053d97624
MarJC5 <jmartin@student.42lausanne.ch>
2022-09-23 12:06:34 +0200

    new v

--- Rank04/v2/microshell.c
+++ Rank04/v2/microshell.c
@@ -1,96 +1,111 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-void	ft_putstr_err(char *str)
+void print_err(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	write(2, str, i);
+    int i = 0;
+    while (str[i])
+        i++;
+    write(2, str, i);
 }
 
-int	ft_execute(char **argv, int i, int tmp_fd, char **env)
+int main(int argc, char **argv, char **envp)
 {
-	argv[i] = NULL;
-	close(tmp_fd);
-	execve(argv[0], argv, env);
-	ft_putstr_err("error: cannot execute ");
-	ft_putstr_err(argv[0]);
-	write(2, "\n", 1);
-	return (1);
-}
+	if (argc == 1)
+		return (0);
 
-int	main(int argc, char **argv, char **env)
-{
-	int	i;
-	int	tmp_fd;
-	int	fd[2];
-	int	pid;
-
-	(void)argc;
-	i = 0;
-	pid = 0;
-	tmp_fd = dup(STDIN_FILENO);
-	while (argv[i] && argv[i + 1])
+	int count = 1;
+	for (int i = 0; argv[i]; i++)
 	{
-		argv = &argv[i + 1];
-		i = 0;
-		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
-			i++;
-		if (strcmp(argv[0], "cd") == 0)
+		if (!strcmp(argv[i], "|") || !strcmp(argv[i], ";"))
 		{
-			if (i != 2)
-				ft_putstr_err("error: cd: bad arguments\n");
-			else if (chdir(argv[1]) != 0)
-			{
-				ft_putstr_err("error: cd: cannot execute ");
-				ft_putstr_err(argv[1]);
-				write(2, "\n", 1);
-			}
+			count++;
+			while (!strcmp(argv[i], ";"))
+				i++;
+			if (!strcmp(argv[i - 1], ";"))
+				i--;
 		}
-		else if (argv != &argv[i] && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
-		{
-			pid = fork();
-			if (pid == 0)
-			{
-				dup2(tmp_fd, STDIN_FILENO);
-				if (ft_execute(argv, i, tmp_fd, env))
-					return (1);
-			}
-			else
-			{
-				close(tmp_fd);
-				waitpid(-1, NULL, WUNTRACED);
-				tmp_fd = dup(STDIN_FILENO);
-			}
-		}
-		else if (argv != &argv[i] && strcmp(argv[i], "|") == 0)
-		{
-			pipe (fd);
-			pid = fork();
-			if (pid == 0)
-			{
-				dup2(tmp_fd, STDIN_FILENO);
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[1]);
-				close(tmp_fd);
-				if (ft_execute(argv, i, tmp_fd, env))
-					return (1);
-			}
-			else
-			{
-				close(tmp_fd);
-				close(fd[1]);
-				waitpid(-1, NULL, WUNTRACED);
-				tmp_fd = dup(fd[0]);
-				close(fd[0]);
-			}
-		}
-
 	}
-	close(tmp_fd);
+
+	bool type[count];
+	int pos[count];
+
+	for (int i = 0; i < count; i++)
+		type[i] = 0;
+	pos[0] = 1;
+
+	int j = 0;
+	for (int i = 0; argv[i]; i++)
+	{
+		if (!strcmp(argv[i], "|"))
+		{
+			pos[j + 1] = i + 1;
+			type[j++] = 1;
+			argv[i] = NULL;
+		}
+		else if (!strcmp(argv[i], ";"))
+		{
+			while (!strcmp(argv[i], ";"))
+				argv[i++] = NULL;
+			pos[1 + j++] = i;
+		}
+	}
+
+	int fd[2];
+	int fdd = 0;
+	int pid;
+
+	for (int i = 0; i < count; i++)
+	{
+		if (!strcmp(argv[pos[i]], "cd"))
+		{
+			if (argv[pos[i] + 2])
+			{
+				print_err("error: cd: bad arguments\n");
+				return (1);
+			}
+			else if (chdir(argv[pos[i] + 1]))
+			{
+				print_err("error: cd: cannot change directory to ");
+				print_err(argv[pos[i] + 1]);
+				print_err("\n");
+			}
+		}
+		else
+		{
+			if (pipe(fd) == -1)
+			{
+				print_err("error: fatal\n");
+				exit(1);
+			}
+			if ((pid = fork()) == -1)
+			{
+				print_err("error: fatal\n");
+				exit(1);
+			}
+			if (!pid)
+			{
+				dup2(fdd, 0);
+				if (type[i] == 1)
+					dup2(fd[1], 1);
+				if (execve(argv[pos[i]], &argv[pos[i]], envp) == -1)
+				{
+					print_err("error: cannot execute ");
+					print_err(argv[pos[i]]);
+					print_err("\n");
+				}
+			}
+			else
+			{
+				waitpid(pid, NULL, 0);
+				close(fd[1]);
+				close(fdd);
+				fdd = fd[0];
+			}
+		}
+	}
 	return (0);
 }
