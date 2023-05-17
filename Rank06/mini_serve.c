252289a96b72fb5c457a12fc8cc0c1f8901092fc
MarJC5 <admin@marjc.ch>
2023-05-17 07:21:55 +0200

    feat(exam06): refacto

--- Rank06/mini_serve.c
+++ Rank06/mini_serve.c
@@ -32,12 +32,8 @@ void sendAll(int es)
 	}
 }
 
-int main(int argc, char **argv)
+int create_server_socket(struct sockaddr_in addr)
 {
-	if (argc != 2)
-		exitError("Wrong number of arguments\n");
-	int port = atoi(argv[1]);
-
 	int serverSock = socket(AF_INET, SOCK_STREAM, 0);
 	if (serverSock < 0)
 		exitError("Fatal error\n");
@@ -48,16 +44,67 @@ int main(int argc, char **argv)
 	max = serverSock;
 	FD_SET(serverSock, &active);
 
+	if ((bind(serverSock, (const struct sockaddr *)&addr, sizeof(addr))) < 0)
+		exitError("Fatal error\n");
+	if (listen(serverSock, 128) < 0)
+		exitError("Fatal error\n");
+
+	return serverSock;
+}
+
+int handle_new_client(int serverSock, struct sockaddr_in addr, socklen_t addr_len)
+{
+	int clientSocket = accept(serverSock, (struct sockaddr *)&addr, &addr_len);
+	if (clientSocket < 0)
+		return -1;
+	max = (clientSocket > max) ? clientSocket : max;
+	clients[clientSocket].id = next_id++;
+	FD_SET(clientSocket, &active);
+	sprintf(bufWrite, "server: client %d just arrived\n", clients[clientSocket].id);
+	sendAll(clientSocket);
+	return 0;
+}
+
+void handle_client_message(int fd)
+{
+	int read = recv(fd, bufRead, 424242, 0);
+	if (read <= 0)
+	{
+		sprintf(bufWrite, "server: client %d just left\n", clients[fd].id);
+		sendAll(fd);
+		FD_CLR(fd, &active);
+		close(fd);
+	}
+	else
+	{
+		for (int i = 0, j = strlen(clients[fd].msg); i < read; i++, j++)
+		{
+			clients[fd].msg[j] = bufRead[i];
+			if (clients[fd].msg[j] == '\n')
+			{
+				clients[fd].msg[j] = '\0';
+				sprintf(bufWrite, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+				sendAll(fd);
+				bzero(&clients[fd].msg, strlen(clients[fd].msg));
+				j = -1;
+			}
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		exitError("Wrong number of arguments\n");
+	int port = atoi(argv[1]);
+
 	struct sockaddr_in addr;
 	socklen_t addr_len = sizeof(addr);
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = htonl(2130706433);
 	addr.sin_port = htons(port);
 
-	if ((bind(serverSock, (const struct sockaddr *)&addr, sizeof(addr))) < 0)
-		exitError("Fatal error\n");
-	if (listen(serverSock, 128) < 0)
-		exitError("Fatal error\n");
+	int serverSock = create_server_socket(addr);
 
 	while(1)
 	{
@@ -69,43 +116,14 @@ int main(int argc, char **argv)
 		{
 			if (FD_ISSET(fd, &readyRead) && fd == serverSock)
 			{
-				int clientSocket = accept(serverSock, (struct sockaddr *)&addr, &addr_len);
-				if (clientSocket < 0)
+				if (handle_new_client(serverSock, addr, addr_len) < 0)
 					continue;
-				max = (clientSocket > max) ? clientSocket : max;
-				clients[clientSocket].id = next_id++;
-				FD_SET(clientSocket, &active);
-				sprintf(bufWrite, "server: client %d just arrived\n", clients[clientSocket].id);
-				sendAll(clientSocket);
 				break;
 			}
 			if (FD_ISSET(fd, &readyRead) && fd != serverSock)
 			{
-				int read = recv(fd, bufRead, 424242, 0);
-				if (read <= 0)
-				{
-					sprintf(bufWrite, "server: client %d just left\n", clients[fd].id);
-					sendAll(fd);
-					FD_CLR(fd, &active);
-					close(fd);
-					break;
-				}
-				else
-				{
-					for (int i = 0, j = strlen(clients[fd].msg); i < read; i++, j++)
-					{
-						clients[fd].msg[j] = bufRead[i];
-						if (clients[fd].msg[j] == '\n')
-						{
-							clients[fd].msg[j] = '\0';
-							sprintf(bufWrite, "client %d: %s\n", clients[fd].id, clients[fd].msg);
-							sendAll(fd);
-							bzero(&clients[fd].msg, strlen(clients[fd].msg));
-							j = -1;
-						}
-					}
-					break;
-				}
+				handle_client_message(fd);
+				break;
 			}
 		}
 	}
